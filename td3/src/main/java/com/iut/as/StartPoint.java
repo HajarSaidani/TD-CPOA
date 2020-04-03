4c8347d4b74da760696162b875ce95c642984648
Eldenn <Eldenn@192.168.0.28>
2020-04-03 12:35:36 +0200

    push

--- td3/src/main/java/com/iut/as/StartPoint.java
+++ td3/src/main/java/com/iut/as/StartPoint.java
@@ -5,14 +5,14 @@ import com.iut.as.modele.Mathematic;
 public class StartPoint {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World TD2");
+		//System.out.println("Hello World TD2");
 		Mathematic maths = new Mathematic();
-		System.out.println("Le resultat de 2 + 4 est " + maths.addition(2, 4));
-		try {
-			System.out.println("Le resultat de 4 / 2 est " + maths.division(4, 2));
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
+		System.out.println("Le resultat de 2 + 3 est " + maths.addition(2, 3));
+		//try {
+		//	System.out.println("Le resultat de 4 / 2 est " + maths.division(4, 2));
+		//} catch (Exception e) {
+		//	System.out.println(e.getMessage());
+		//}
 	}
 
 }
