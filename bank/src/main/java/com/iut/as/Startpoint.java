f5cda8254151b2020446a79559ce688af88f2c9d
Eldenn <kevinscythes@gmail.com>
2020-04-30 09:54:47 +0200

    Connexion

---
+++ bank/src/main/java/com/iut/as/Startpoint.java
@@ -0,0 +1,5 @@
+package com.iut.as;
+
+public class Startpoint {
+
+}
--- bank/src/main/java/com/iut/as/dao/factory/Connexion.java
+++ bank/src/main/java/com/iut/as/dao/factory/Connexion.java
@@ -4,16 +4,26 @@ import java.sql.*;
 
 
 public class Connexion {
-	public Connection creeConnexion() {
-		String url = "jdbc:mysql://devbdd.iutmetz.univ-lorraine.fr:3306/saidani3u_bank";
-		String login = "saidani3u_appli";
-		String pwd = "31118884"; 
-		Connection maConnexion = null;
+
+		private String url = "jdbc:mysql://devbdd.iutmetz.univ-lorraine.fr:3306/saidani3u_bank";
+		private String login = "saidani3u_appli";
+		private String pwd = "31118884"; 
+		
+		private static Connection maConnexion;
+		
 		try { 
 			maConnexion = DriverManager.getConnection(url, login, pwd);
 			} catch (SQLException sqle) {
 				System.out.println("Erreur connexion" + sqle.getMessage());
 				}
+		
+		public static Connection getInstance(){
+	        if(maConnexion == null){
+	            new Connexion();
+	            System.out.println("INSTANCIATION DE LA CONNEXION SQL ! ");
+	        }
+		}
+		
 		return maConnexion;
 	}
 	
